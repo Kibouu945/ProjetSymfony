8240ff02a409269757eb70d543c697a8f7cba43c
roktadj <rokibtadjou17@gmail.com>
2023-07-04 15:02:27 +0200

    New Changes - Réservation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -25,9 +25,34 @@ class HomeController extends AbstractController
     {
         return $this->render('home/about.html.twig');
     }
+    
     #[Route('/reservation', name: 'reservation')]
     public function reservation(): Response
     {
+
+         /*
+      
+        // Vérifier si l'utilisateur est connecté
+        if ($this->getUser()) {
+            // L'utilisateur est déjà connecté, vous pouvez traiter la réservation ici
+
+            $nom = $request->request->get('nom');
+            $prenom = $request->request->get('prenom');
+            $dateReservation = $request->request->get('date_reservation');
+            $typeSalon = $request->request->get('type_salon');
+            $dureeReservation = $request->request->get('duree_reservation');
+            $typeForfait = $request->request->get('type_forfait');
+
+            // Redirection vers la page de confirmation de réservation
+            return $this->redirectToRoute('reservation_confirmation');
+        }
+
+        // L'utilisateur n'est pas connecté, redirection vers la page de connexion
+        return $this->redirectToRoute('app_login');
+
+        
+      */ 
+        
         return $this->render('home/reservation.html.twig');
     }
     
