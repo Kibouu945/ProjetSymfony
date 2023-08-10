ee57507cb0840ed5624c924e2730f1f3a16ed561
OHIN A. Landry <54063351+Landryohin@users.noreply.github.com>
2023-08-10 23:20:38 +0200

    reset password

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -25,21 +25,38 @@ class ChangePasswordFormType extends AbstractType
                 'first_options' => [
                     'constraints' => [
                         new NotBlank([
+<<<<<<< HEAD
                             'message' => 'Veuillez entrer un mot de passe',
                         ]),
                         new Length([
                             'min' => 6,
                             'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
+=======
+                            'message' => 'Please enter a password',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+>>>>>>> addde7a (reset password)
                             // max length allowed by Symfony for security reasons
                             'max' => 4096,
                         ]),
                     ],
+<<<<<<< HEAD
                     'label' => 'Nouveau mot de passe',
                 ],
                 'second_options' => [
                     'label' => 'Répéter le mot de passe',
                 ],
                 'invalid_message' => 'Les champs du mot de passe doivent correspondre.',
+=======
+                    'label' => 'New password',
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                ],
+                'invalid_message' => 'The password fields must match.',
+>>>>>>> addde7a (reset password)
                 // Instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
