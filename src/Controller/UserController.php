aca5fe868aeac7c9c06d466b53b3a8e30a5430e6
roktadj <rokibtadjou17@gmail.com>
2023-08-21 01:15:35 +0200

    Updating

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,7 +9,8 @@ use App\Entity\User;
 use App\Form\ChangePasswordFormType;
 use Doctrine\ORM\EntityManagerInterface; 
 use Symfony\Component\HttpFoundation\Request; 
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface; 
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 
 class UserController extends AbstractController
@@ -17,7 +18,6 @@ class UserController extends AbstractController
     #[Route('/page-utilisateur', name: 'user_homepage')]
     public function index()
     {
-
         $user = $this->getUser();
 
         if (!$user) {
@@ -28,47 +28,6 @@ class UserController extends AbstractController
             'user' => $user,
         ]);
     }
-
-
-
-    // #[Route('/utilisateur/edition-mot-de-passe/{id}', 'user.edit.password', methods: ['GET', 'POST'])]
-    // public function editPassword(
-    //     User $choosenUser,
-    //     Request $request,
-    //     EntityManagerInterface $manager,
-    //     UserPasswordHasherInterface $hasher
-    // ): Response {
-    //     $form = $this->createForm(ChangePasswordFormType::class );
-
-    //     $form->handleRequest($request);
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         if ($hasher->isPasswordValid($choosenUser, $form->getData()['plainPassword'])) { 
-    //             $choosenUser->
-    //                 $form->getData()['newPassword']
-    //             ;
-
-    //             $this->addFlash(
-    //                 'success',
-    //                 'Le mot de passe a été modifié.'
-    //             );
-
-    //             $manager->persist($choosenUser);
-    //             $manager->flush();
-
-    //             return $this->redirectToRoute('user_homepage');
-    //         } else {
-    //             $this->addFlash(
-    //                 'warning',
-    //                 'Le mot de passe renseigné est incorrect.'
-    //             );
-    //         }
-    //     }
-
-    //     return $this->render('user/update.html.twig', [
-    //         'form' => $form->createView()
-    //     ]);
-    // }
-
 
     #[Route('/utilisateur/edition-mot-de-passe/{id}', name: 'user.edit.password', methods: ['GET', 'POST'])]
     #[IsGranted("ROLE_USER")] 
@@ -83,7 +42,6 @@ class UserController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $choosenUser; // Use the injected $choosenUser directly
-            
 
             $plainPassword = $form->get('newPassword')->getData();
  
@@ -92,8 +50,6 @@ class UserController extends AbstractController
                 $hashedPassword = $hasher->hashPassword($user, $plainPassword);
                 $user->setPassword($hashedPassword);
             }
-
-         
 
             $manager->flush();
 
@@ -112,6 +68,29 @@ class UserController extends AbstractController
     }
 
 
+    #[Route('/utilisateur/suppression-profil/{id}', name: 'user.delete', methods: ['GET', 'POST'])]
+    public function delete(EntityManagerInterface $entityManager , User $user): Response
+    {
+        // Vérification si l'utilisateur est connecté
+        $currentUser = $this->getUser();
+        if (!$currentUser) {
+            throw new AccessDeniedException('Vous devez être connecté pour accéder à cette section.');
+        }
 
+        // Vérification si l'utilisateur a le droit de supprimer ce profil (c'est son profil ou il est admin)
+        if ($currentUser->getId() !== $user->getId() && !$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException('Vous n\'avez pas le droit de supprimer ce profil.');
+        }
 
+        // Si tout va bien, supprimez le profil
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        // Ajouter un flash message pour notifier l'utilisateur
+        $this->addFlash('success', 'Profil supprimé avec succès.');
+
+        // Rediriger vers la page d'accueil ou de connexion après la suppression
+        return $this->redirectToRoute('app_login');
+    }
 }
+
