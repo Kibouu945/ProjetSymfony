e406b8717c9945d89e7b79dfc47b4bee39a16e87
roktadj <rokibtadjou17@gmail.com>
2023-09-01 12:05:00 +0200

    Cookies For Website

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
@@ -46,5 +48,16 @@ class HomeController extends AbstractController
         return $this->render('home/mentionslegales.html.twig');
     }
 
+    /*
+    #[Route('/app_home_set-cookie', name: 'app_home_set-cookie')]
+    public function acceptCookies(Request $request): Response {
+        // Set a cookie to indicate that cookies have been accepted
+        $response = new Response();
+        $response->headers->setCookie(new Cookie('cookies_accepted', '1', time() + (365 * 24 * 60 * 60))); // expires in 1 year
+
+        $referer = $request->headers->get('referer');
+        return $response->setStatusCode(Response::HTTP_FOUND)->headers->set('Location', $referer);
+    }
+    */
 
 }
