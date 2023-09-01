30ec8518fb8498ac5dc89d68881bf88100ac12f7
Gaillard05 <maylisgaillard@hotmail.fr>
2023-09-01 09:41:32 +0200

    add project

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -7,10 +7,15 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+<<<<<<< HEAD
 use App\Form\Assert\NotBlank;
 use Symfony\Component\Validator\Constraints\Length; 
 use Symfony\Component\Form\Extension\Core\Type\SubmitType; 
 
+=======
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+>>>>>>> addde7a (reset password)
 
 class ChangePasswordFormType extends AbstractType
 {
@@ -19,6 +24,7 @@ class ChangePasswordFormType extends AbstractType
         $builder
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
+<<<<<<< HEAD
                 'first_options' => [
                     'attr' => [
                         'class' => 'form-control'
@@ -51,6 +57,36 @@ class ChangePasswordFormType extends AbstractType
                 'label' => 'Changer mon mot de passe'
             ]);
 
+=======
+                'options' => [
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'first_options' => [
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Please enter a password',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+                    'label' => 'New password',
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                ],
+                'invalid_message' => 'The password fields must match.',
+                // Instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+            ])
+        ;
+>>>>>>> addde7a (reset password)
     }
 
     public function configureOptions(OptionsResolver $resolver): void
